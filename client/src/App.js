b586fef1ce16e7890531f4c5b06ae0fe88deecdc
Nishant garg <iamnishantgarg@gmail.com>
2020-10-12 15:27:16 +0530

    nav and products template

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to shopee</h1>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
