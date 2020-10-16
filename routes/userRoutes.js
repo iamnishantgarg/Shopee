a1a7ed11a83945c4103b0109ab2144a0afe61c8f
Nishant garg <iamnishantgarg@gmail.com>
2020-10-16 19:54:40 +0530

    user register done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
-const { authUser, getProfile } = require("../controllers/userController");
+const {
+  authUser,
+  getProfile,
+  registerUser,
+} = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/login", authUser);
 router.get("/profile", protect, getProfile);
+router.post("/", registerUser);
 
 module.exports = router;
